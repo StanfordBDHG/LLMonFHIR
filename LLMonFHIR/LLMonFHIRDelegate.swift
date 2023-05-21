269444cebcb408aca6a01a83f8fb8f252b68979f
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-05-20 19:47:04 -0700

    Removes the Firebase Setup and Dependencies (#2)

--- LLMonFHIR/LLMonFHIRDelegate.swift
+++ LLMonFHIR/LLMonFHIRDelegate.swift
@@ -6,17 +6,10 @@
 // SPDX-License-Identifier: MIT
 //
 
+import HealthKit
 import Spezi
 import SpeziFHIR
 import SpeziFHIRMockDataStorageProvider
-import SpeziFHIRToFirestoreAdapter
-import SpeziFirebaseAccount
-import class FirebaseFirestore.FirestoreSettings
-import class FirebaseFirestore.MemoryCacheSettings
-import FirebaseAuth
-import HealthKit
-import SpeziFirestore
-import SpeziFirestorePrefixUserIdAdapter
 import SpeziHealthKit
 import SpeziHealthKitToFHIRAdapter
 import SpeziQuestionnaire
@@ -27,14 +20,6 @@ import SwiftUI
 class LLMonFHIRDelegate: SpeziAppDelegate {
     override var configuration: Configuration {
         Configuration(standard: FHIR()) {
-            if !FeatureFlags.disableFirebase {
-                if FeatureFlags.useFirebaseEmulator {
-                    FirebaseAccountConfiguration(emulatorSettings: (host: "localhost", port: 9099))
-                } else {
-                    FirebaseAccountConfiguration()
-                }
-                firestore
-            }
             if HKHealthStore.isHealthDataAvailable() {
                 healthKit
             }
@@ -42,24 +27,6 @@ class LLMonFHIRDelegate: SpeziAppDelegate {
             MockDataStorageProvider()
             LLMonFHIRScheduler()
         }
-    }
-    
-    
-    private var firestore: Firestore<FHIR> {
-        let settings = FirestoreSettings()
-        if FeatureFlags.useFirebaseEmulator {
-            settings.host = "localhost:8080"
-            settings.cacheSettings = MemoryCacheSettings()
-            settings.isSSLEnabled = false
-        }
-        
-        return Firestore(
-            adapter: {
-                FHIRToFirestoreAdapter()
-                FirestorePrefixUserIdAdapter()
-            },
-            settings: settings
-        )
     }
     
     
--- LLMonFHIR/SharedContext/FeatureFlags.swift
+++ LLMonFHIR/SharedContext/FeatureFlags.swift
@@ -12,13 +12,4 @@ enum FeatureFlags {
     static let skipOnboarding = CommandLine.arguments.contains("--skipOnboarding")
     /// Always show the onboarding when the application is launched. Makes it easy to modify and test the onboarding flow without the need to manually remove the application or reset the simulator.
     static let showOnboarding = CommandLine.arguments.contains("--showOnboarding")
-    /// Disables the Firebase interactions, including the login/sign-up step and the Firebase Firestore upload.
-    static let disableFirebase = CommandLine.arguments.contains("--disableFirebase")
-    #if targetEnvironment(simulator)
-    /// Defines if the application should connect to the local firebase emulator. Always set to true when using the iOS simulator.
-    static let useFirebaseEmulator = true
-    #else
-    /// Defines if the application should connect to the local firebase emulator. Always set to true when using the iOS simulator.
-    static let useFirebaseEmulator = CommandLine.arguments.contains("--useFirebaseEmulator")
-    #endif
 }
