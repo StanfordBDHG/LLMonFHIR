9f70a41c9616a497f6716d6d29ba37f4e5fc2d97
Adrit Rao <adrit.rao@gmail.com>
2023-07-10 12:31:58 -0700

    Multiple Resource Interpreter fixed

---
+++ LLMonFHIR/FHIR Display/FHIRMultipleResourceInterpreter.swift
@@ -0,0 +1,103 @@
+//
+// This source file is part of the Stanford LLM on FHIR project
+//
+// SPDX-FileCopyrightText: 2023 Stanford University
+//
+// SPDX-License-Identifier: MIT
+//
+
+import OpenAI
+import Spezi
+import SpeziLocalStorage
+import SpeziOpenAI
+import SwiftUI
+
+
+private enum FHIRMultipleResourceInterpreterConstants {
+    static let storageKey = "FHIRMultipleResourceInterpreter.Cache"
+}
+
+
+class FHIRMultipleResourceInterpreter<ComponentStandard: Standard>: DefaultInitializable, Component, ObservableObject, ObservableObjectProvider {
+    
+    @Dependency private var localStorage: LocalStorage
+    @Dependency private var openAIComponent = OpenAIComponent()
+    
+//    var multipleResourceInterpretation = ""
+    typealias multipleResourceInterpretation = String
+
+    
+    
+    var interpretations: multipleResourceInterpretation = "" {
+        willSet {
+            Task { @MainActor in
+                objectWillChange.send()
+            }
+        }
+        didSet {
+            do {
+                try localStorage.store(interpretations, storageKey: FHIRMultipleResourceInterpreterConstants.storageKey)
+            } catch {
+                print(error)
+            }
+        }
+    }
+    
+    
+    required init() {}
+    
+    
+    func configure() {
+        guard let cachedInterpretation: multipleResourceInterpretation = try? localStorage.read(storageKey: FHIRMultipleResourceInterpreterConstants.storageKey) else {
+            return
+        }
+
+        self.interpretations = cachedInterpretation
+    }
+    
+    
+    func interpretMultipleResources(resources: [FHIRResource]) async throws {
+
+        let chatStreamResults = try await openAIComponent.queryAPI(withChat: [systemPrompt(forResources: resources)])
+        
+        interpretations = ""
+        
+        for try await chatStreamResult in chatStreamResults {
+            for choice in chatStreamResult.choices {
+                let previousInterpretation = interpretations ?? ""
+                interpretations = previousInterpretation + (choice.delta.content ?? "")
+            }
+        }
+    }
+    
+    func chat(resources: [FHIRResource]) -> [Chat] {
+        var chat = [systemPrompt(forResources: resources)]
+        
+        if let interpretation: String? = interpretations {
+            if let interpretation = interpretation {
+                chat.append(Chat(role: .assistant, content: interpretation))
+            }
+        }
+        
+        return chat
+    }
+
+    
+    private func systemPrompt(forResources resources: [FHIRResource]) -> Chat {
+        
+        var allJSONResources = ""
+        
+        for resource in resources {
+            allJSONResources = allJSONResources + "\n" + resource.compactJSONDescription
+        }
+        
+        print("JSON: "+allJSONResources)
+        
+        
+        return Chat(
+            role: .system,
+            content: Prompt.interpret_multiple_resources.prompt.replacingOccurrences(of: Prompt.promptPlaceholder, with: allJSONResources)
+            
+        )
+    }
+}
