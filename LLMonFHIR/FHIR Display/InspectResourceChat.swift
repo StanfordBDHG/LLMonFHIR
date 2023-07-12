9ed3a1a7ab5123406650e0872e53a690cc61fb41
Adrit Rao <adrit.rao@gmail.com>
2023-07-11 17:21:06 -0700

    Build fix v4

--- LLMonFHIR/FHIR Display/InspectResourceChat.swift
+++ LLMonFHIR/FHIR Display/InspectResourceChat.swift
@@ -52,10 +52,9 @@ struct InspectResourceChat: View {
                     for choice in chatStreamResult.choices {
                         if chat.last?.role == .assistant {
                             let previousChatMessage = chat.last ?? Chat(role: .assistant, content: "")
-                            let deltaContent = choice.delta.content ?? ""
                             chat[chat.count - 1] = Chat(
                                 role: .assistant,
-                                content: previousChatMessage.content! + deltaContent
+                                content: (previousChatMessage.content ?? "") + (choice.delta.content ?? "")
                             )
                         } else {
                             chat.append(Chat(role: .assistant, content: choice.delta.content ?? ""))
