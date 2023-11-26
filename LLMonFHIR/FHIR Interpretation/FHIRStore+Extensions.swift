a24b5bc070cf9f0dfe6cefeadf2e1a29f83f7d70
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-11-26 15:26:16 +0100

    FHIR 0.5.2 and Improved Resource Handling (#39)
    # FHIR 0.5.2 and Improved Resource Handling
    
    ## :gear: Release Notes 
    - Update to Spezi FHIR 0.5.2 and all included improvements:
    https://github.com/StanfordSpezi/SpeziFHIR/releases/tag/0.5.2
    - Display the data in the overview
    - Improve the resource selection and reenable the resource limit
    - Improve the prompts and resource selection
    
    
    ### Code of Conduct & Contributing Guidelines 
    
    By submitting creating this pull request, you agree to follow our [Code
    of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md):
    - [x] I agree to follow the [Code of
    Conduct](https://github.com/StanfordBDHG/.github/blob/main/CODE_OF_CONDUCT.md)
    and [Contributing
    Guidelines](https://github.com/StanfordBDHG/.github/blob/main/CONTRIBUTING.md).

--- LLMonFHIR/FHIR Interpretation/FHIRStore+Extensions.swift
+++ LLMonFHIR/FHIR Interpretation/FHIRStore+Extensions.swift
@@ -8,6 +8,7 @@
 
 import ModelsR4
 import SpeziFHIR
+import SwiftUI
 
 
 extension FHIRStore {
@@ -16,7 +17,26 @@ extension FHIRStore {
     }
     
     var allResourcesFunctionCallIdentifier: [String] {
-        Array(Set(allResources.map { $0.functionCallIdentifier }))
+        @AppStorage(StorageKeys.resourceLimit) var resourceLimit = StorageKeys.Defaults.resourceLimit
+        
+        let relevantResources: [FHIRResource]
+        if allResources.count > resourceLimit {
+            var limitedResources: [FHIRResource] = []
+            limitedResources.append(contentsOf: allergyIntolerances.dateSuffix(maxLength: resourceLimit / 9))
+            limitedResources.append(contentsOf: conditions.dateSuffix(maxLength: resourceLimit / 9))
+            limitedResources.append(contentsOf: diagnostics.dateSuffix(maxLength: resourceLimit / 9))
+            limitedResources.append(contentsOf: encounters.dateSuffix(maxLength: resourceLimit / 9))
+            limitedResources.append(contentsOf: immunizations.dateSuffix(maxLength: resourceLimit / 9))
+            limitedResources.append(contentsOf: medications.dateSuffix(maxLength: resourceLimit / 9))
+            limitedResources.append(contentsOf: observations.dateSuffix(maxLength: resourceLimit / 9))
+            limitedResources.append(contentsOf: otherResources.dateSuffix(maxLength: resourceLimit / 9))
+            limitedResources.append(contentsOf: procedures.dateSuffix(maxLength: resourceLimit / 9))
+            relevantResources = limitedResources
+        } else {
+            relevantResources = allResources
+        }
+        
+        return Array(Set(relevantResources.map { $0.functionCallIdentifier }))
     }
     
     
@@ -36,5 +56,12 @@ extension FHIRStore {
             removeAllResources()
             insert(resource: mockFHIRResource)
         }
+    }
+}
+
+
+extension Array where Element == FHIRResource {
+    fileprivate func dateSuffix(maxLength: Int) -> [FHIRResource] {
+        self.lazy.sorted(by: { $0.date ?? .distantPast < $1.date ?? .distantPast }).suffix(maxLength)
     }
 }
