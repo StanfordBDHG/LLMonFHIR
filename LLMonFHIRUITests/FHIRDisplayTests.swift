6f55483bfc5a7bc2f594c844151f4f256bbd19ab
Paul Schmiedmayer <PSchmiedmayer@users.noreply.github.com>
2023-12-14 19:38:01 -0800

    Update Patients

--- LLMonFHIRUITests/FHIRDisplayTests.swift
+++ LLMonFHIRUITests/FHIRDisplayTests.swift
@@ -42,12 +42,12 @@ final class FHIRDisplayTests: XCTestCase {
         let app = XCUIApplication()
         
         let patients = [
-            "Allen322 Ferry570",
-            "Beatris270 Bogan287",
-            "Edythe31 Morar593",
-            "Gonzalo160 Dueñas839",
-            "Jacklyn830 Veum823",
-            "Milton509 Ortiz186"
+            "Allen322 Ferry570", // @Paul
+            "Beatris270 Bogan287", // @Paul
+            "Edythe31 Morar593", // @Paul
+            "Gonzalo160 Dueñas839", // @Vishnu
+            "Jacklyn830 Veum823", // @Vishnu
+            "Milton509 Ortiz186" // @Vishnu
         ]
         
         let questions = [
