53077264bd21e59304ef958d2cab9ef25c51c127
Lukas Kollmer <hey@lukaskollmer.de>
2025-12-09 16:53:34 +0100

    rework the QRCode gen a bit

--- LLMonFHIR/Study Flow/CreateEnrollmentQRCodeSheet.swift
+++ LLMonFHIR/Study Flow/CreateEnrollmentQRCodeSheet.swift
@@ -12,12 +12,16 @@ import SwiftUI
 
 
 struct CreateEnrollmentQRCodeSheet: View {
-    @State private var studyId: Survey.ID?
-    @State private var enableExpirationTimestamp = true
-    @State private var expirationDate = Calendar.current.startOfNextDay(for: .now) - 30
-    @State private var correctionLevel: QRCodeGenerator.CorrectionLevel = .high
+    private struct CodeGenerationOptions: Equatable {
+        var studyId: Survey.ID?
+        var expires = true
+        var validDuration: Duration = .seconds(5)
+        var correctionLevel: QRCodeGenerator.CorrectionLevel = .high
+    }
     
+    @State private var options = CodeGenerationOptions()
     @State private var qrCode: Image?
+    @State private var imageRefreshTask: Task<Void, any Error>?
     
     var body: some View {
         NavigationStack {
@@ -34,10 +38,22 @@ struct CreateEnrollmentQRCodeSheet: View {
             .navigationTitle("Create Study Enrollment QR Code")
             .navigationBarTitleDisplayMode(.inline)
         }
-        .onChange(of: studyId) { updateQRCode() }
-        .onChange(of: enableExpirationTimestamp) { updateQRCode() }
-        .onChange(of: expirationDate) { updateQRCode() }
-        .onChange(of: correctionLevel) { updateQRCode() }
+        .onChange(of: options) { _, options in
+            updateQRCode()
+            imageRefreshTask?.cancel()
+            imageRefreshTask = Task {
+                guard options.expires else {
+                    return
+                }
+                while true {
+                    try? await Task.sleep(for: options.validDuration / 2)
+                    guard !Task.isCancelled else {
+                        return
+                    }
+                    updateQRCode()
+                }
+            }
+        }
     }
     
     @ViewBuilder private var leftColumn: some View {
@@ -49,15 +65,15 @@ struct CreateEnrollmentQRCodeSheet: View {
                     .scaledToFit()
                     .frame(width: 600, height: 600)
             } else {
-                Text("No Image")
+                Text("Select a study on the right.")
             }
         }
     }
     
     @ViewBuilder private var rightColumn: some View {
-        Form {
+        Form { // swiftlint:disable:this closure_body_length
             Section {
-                Picker("Study", selection: $studyId) {
+                Picker("Study", selection: $options.studyId) {
                     Text("–")
                         .tag(Survey.ID?.none)
                         .selectionDisabled()
@@ -69,13 +85,21 @@ struct CreateEnrollmentQRCodeSheet: View {
                 }
             }
             Section {
-                Toggle("Expires", isOn: $enableExpirationTimestamp)
-                if enableExpirationTimestamp {
-                    DatePicker("Expiration Date", selection: $expirationDate)
+                Toggle("Expires", isOn: $options.expires)
+                if options.expires {
+                    Picker("Valid for", selection: $options.validDuration) {
+                        let options: [Duration] = [.seconds(5), .seconds(30)]
+                        ForEach(options, id: \.self) { option in
+                            Text(option, format: Duration.UnitsFormatStyle(allowedUnits: [.hours, .minutes, .seconds], width: .narrow))
+                                .tag(option)
+                        }
+                    }
                 }
+            } footer: {
+                Text("STUDY_QR_CODE_GEN_EXPIRATION_FOOTER")
             }
             Section("Other") {
-                Picker("Correction Level", selection: $correctionLevel) {
+                Picker("Correction Level", selection: $options.correctionLevel) {
                     Text("Low")
                         .tag(QRCodeGenerator.CorrectionLevel.low)
                     Text("Medium")
@@ -90,19 +114,18 @@ struct CreateEnrollmentQRCodeSheet: View {
     }
     
     private func updateQRCode() {
-        guard let studyId else {
+        guard let studyId = options.studyId else {
             qrCode = nil
             return
         }
         let payload = CurrentStudyManager.QRCodePayload(
             studyId: studyId,
-//            expires: enableExpirationTimestamp ? .now + validity.timeInterval : nil
-            expires: enableExpirationTimestamp ? expirationDate : nil
+            expires: options.expires ? .now + options.validDuration.timeInterval : nil
         )
         guard let payload = try? payload.qrCodePayload() else {
             qrCode = nil
             return
         }
-        qrCode = QRCodeGenerator.qrCode(withPayload: payload, correctionLevel: correctionLevel)
+        qrCode = QRCodeGenerator.qrCode(withPayload: payload, correctionLevel: options.correctionLevel)
     }
 }
